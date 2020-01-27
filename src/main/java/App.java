e1ea2b5913d35709cc40d644377dc663955f8dc0
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-27 15:38:33 +0300

    define alphabets which will be used to cipher secrets

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,15 @@
 import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
 //        import and assign inputConsole of type Console
         Console inputConsole = System.console();
+
+        ArrayList<String> alphabet = new ArrayList<String>(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ")); // original alphabet
+        System.out.println(alphabet); //display og alphabet for testing
+        ArrayList<String> caesarAlphabet = new ArrayList<String>(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")); // original alphabet waiting to be exchanged
 
         System.out.println("Tell me secret ;) "); // ask for secret from user
         String wholeSecret= inputConsole.readLine(); //get input
@@ -11,5 +17,7 @@ public class App {
 
         System.out.println("What is your lucky cipher digit? "); //ask for cipher digit
         Integer cipherDigit= Integer.parseInt(inputConsole.readLine()); //get input and parse it immediately
+
+
     }
 }
