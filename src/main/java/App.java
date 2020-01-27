d06fed3e335baea328867d64fc810767d0a2bb0e
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-27 15:30:50 +0300

    import console, add user console interaction

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args) {
+//        import and assign inputConsole of type Console
+        Console inputConsole = System.console();
 
+        System.out.println("Tell me secret ;) "); // ask for secret from user
+        String wholeSecret= inputConsole.readLine(); //get input
+        wholeSecret = wholeSecret.toLowerCase(); //remember to toLowerCase the input
+
+        System.out.println("What is your lucky cipher digit? "); //ask for cipher digit
+        Integer cipherDigit= Integer.parseInt(inputConsole.readLine()); //get input and parse it immediately
     }
 }
